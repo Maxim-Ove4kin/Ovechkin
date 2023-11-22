9d9594ea6b3e1ca20ddcd805d835098496878a84
Maxim-Ove4kin <maks-ovechkin2011@inbox.ru>
2023-11-22 18:04:05 +0300

    a

--- Ovechkin/shalushai/urls.py
+++ Ovechkin/shalushai/urls.py
@@ -3,4 +3,6 @@ from django.urls import path, include
 from .views import *
 urlpatterns = [
     path('', index, name='index'),
+    path('', index, name='in_log_index'),
+    path('', index, name='settings'),
 ]
--- Ovechkin/shalushai/views.py
+++ Ovechkin/shalushai/views.py
@@ -3,4 +3,6 @@ def index(request):
     """Переодресация"""
 
     return render(request, 'salupon/index.html')
+    return render(request, 'salupon/in_log_index.html')
+    return render(request, 'salupon/settings.html')
 # Create your views here.
